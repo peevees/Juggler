518003673d2a00c0ef29d4b14dd1e9bb71d50b6a
Mike <michael.nyberghjerto@outlook.com>
2017-09-13 13:30:26 +0200

    version 1.1
    added faster ball per point  and testing dyamic objects

--- Assets/_Game/Scripts/PlayerControllerButtons.cs
+++ Assets/_Game/Scripts/PlayerControllerButtons.cs
@@ -1,11 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerControllerButtons : MonoBehaviour {
 
     public int direction;
     public PlayerMovement playerMovement;
+    
+    private void Start()
+    {
+
+    }
 
     void OnMouseDown() {
         //Debug.Log("touch recognised" + currentActive);
---
+++ Assets/dynamicController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dynamicController : MonoBehaviour { 
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        for (int i = 0; i < Input.touchCount; ++i)
+        {
+            if (Input.GetTouch(i).phase == TouchPhase.Began)
+            {
+                Debug.Log("" + i);
+                // Construct a ray from the current touch coordinates
+                Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(i).position);
+                Debug.Log(ray);
+            }
+        }
+
+        //Debug.Log (Input.GetTouch().position.ToString);
+        //Vector2 movement = new Vector2();
+    }
+}
